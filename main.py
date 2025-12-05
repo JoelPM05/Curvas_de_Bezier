eb4c547e83cf9d9449f937324a8507cb85a27e2c
JoelPM05 <joel.perez@cimat.mx>
2025-12-05 00:33:20 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ import nodos_puntuales
 pto_inicial = np.array([0,0])
 pto_final = np.array([8,3])
 obs = [np.array([1,2]),np.array([2,0]),np.array([4,2]),np.array([5,5]),np.array([7,2])]
-nod = nodos_puntuales.calcular(pto_inicial,pto_final,obs,1)
-nodos_puntuales.graficar_curva_bezier(nod,obs)
 
 
-def graficar_curva_bezier(nodos):
-    x = np.linspace(0,1,200)
-    puntos = np.array([curva_bezier(xi, nodos) for xi in x])
+def curva_bezier(punto_inicial,punto_final,obstaculos,radio_seguro):
+    nodos = nodos_puntuales.calcular(punto_inicial,punto_final,obstaculos,radio_seguro)
+    nodos_puntuales.graficar_curva_bezier(nodos,obstaculos)
+    return
 
-    xs = puntos[:,0]
-    ys = puntos[:,1]
+def curva_bezier_por_partes(punto_inicial,punto_final,obstaculos,radio_seguro):
+    nodos = nodos_puntuales.calcular(punto_inicial,punto_final,obstaculos,radio_seguro)
 
-    plt.plot(xs, ys, label="Curva Bezier")
-    plt.scatter(*zip(*nodos), color='red', label="Puntos de control")
-    plt.legend()
-    plt.axis('equal')
-    plt.show()
+    n = len(nodos)
+    if n < 5:
+        nodos_puntuales.graficar_curva_bezier(nodos,obstaculos)
+        return
+
+    paquete_nodos = [nodos[:5]] #Se encarga de divir los nodos en paquetes de 5 nodos
+    k = 1
+    while 5*(k+1) <= n:
+        paquete_nodos.append(nodos[k:k+5])
+        k += 1
+    
+    if n- 5*k < 3:
+        paquete_nodos[k-1].concatenate(nodos[5*k:n])
+    else:
+        paquete_nodos.append(nodos[5*k:n])
