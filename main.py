5bad6217f23b31c8a7631689913dd1e14ecc8107
reynaldoDelgado <jose.delgado@cimat.mx>
2025-12-04 20:43:45 -0600

    Update main.py

--- main.py
+++ main.py
@@ -3,17 +3,28 @@ import matplotlib.pyplot as plt
 import math
 
 def curva_bezier(t,puntos_medios): 
+    # Usamos el algoritmo de Casteljau para evaluar la curva de Bezier
     n = len(puntos_medios)
-    suma_final = 0
+    ptos_medios_aprox = puntos_medios.copy()
 
-    for k in range(n):
-        suma_final += ( math.comb(n-1,k) * (t**k) * ((1-t)**(n-1-k))) * puntos_medios[k]
-    
-    return suma_final
+    for j in range (1,n):
+        for k in range(n-j):
+            ptos_medios_aprox[k] = ptos_medios_aprox[k]*(1-t) + ptos_medios_aprox[k+1]*t
+
+    return ptos_medios_aprox[0]
 
 def graficar_curva_bezier(puntos_medios):
-    x = np.linspace(0,1,100)
-    y = [curva_bezier(xi) for xi in x ]
+    x = np.linspace(0,1,200)
+    puntos = np.array([curva_bezier(xi, puntos_medios) for xi in x])
 
-    plt.plot(x,y)
-    return
+    xs = puntos[:,0]
+    ys = puntos[:,1]
+
+    plt.plot(xs, ys, label="Curva Bezier")
+    plt.scatter(*zip(*puntos_medios), color='red', label="Puntos de control")
+    plt.legend()
+    plt.axis('equal')
+    plt.show()
+
+nodos = [np.array([0,0]),np.array([1,3]),np.array([2,2])]
+graficar_curva_bezier(nodos)
