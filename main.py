0fd932b70cb5d5bde37bb7c5c1d80a99e0c34abd
JoelPM05 <joel.perez@cimat.mx>
2025-12-04 21:36:46 -0600

    .

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import nodos_puntuales
 
-def curva_bezier(t,puntos_medios): 
+def curva_bezier(t,nodos): 
     # Usamos el algoritmo de Casteljau para evaluar la curva de Bezier
-    n = len(puntos_medios)
-    ptos_medios_aprox = puntos_medios.copy()
+    n = len(nodos)
+    ptos_medios_aprox = nodos.copy()
 
     for j in range (1,n):
         for k in range(n-j):
@@ -13,18 +14,18 @@ def curva_bezier(t,puntos_medios):
 
     return ptos_medios_aprox[0]
 
-def graficar_curva_bezier(puntos_medios):
+def graficar_curva_bezier(nodos):
     x = np.linspace(0,1,200)
-    puntos = np.array([curva_bezier(xi, puntos_medios) for xi in x])
+    puntos = np.array([curva_bezier(xi, nodos) for xi in x])
 
     xs = puntos[:,0]
     ys = puntos[:,1]
 
     plt.plot(xs, ys, label="Curva Bezier")
-    plt.scatter(*zip(*puntos_medios), color='red', label="Puntos de control")
+    plt.scatter(*zip(*nodos), color='red', label="Puntos de control")
     plt.legend()
     plt.axis('equal')
     plt.show()
 
-nodos = [np.array([0,0]),np.array([1,3]),np.array([2,2])]
-graficar_curva_bezier(nodos)
+pts_medios = [np.array([0,0]),np.array([1,1]),np.array([2,2])]
+#graficar_curva_bezier(pts_medios)
---
+++ nodos_puntuales.py
@@ -0,0 +1,18 @@
+from math import sqrt
+def distancia_recta_punto(punto,punto_inicial,punto_final):
+    A = (punto_inicial[1]-punto_final[1])/(punto_inicial[0]-punto_final[0])
+    B = -1
+    C = punto_inicial[1] - A * punto_inicial[0]
+
+    distancia = abs(A*punto[0] + B*punto[1] + C)/sqrt(A**2 + B**2)
+    print(A)
+    print(B)
+    print(C)
+    return distancia
+
+
+def calcular(punto_inicial,punto_final,obstaculos,radio_seguro):
+    n = len(obstaculos)
+    for k in range(n):
+        if distancia_recta_punto(obstaculos[k],punto_inicial,punto_final) > radio_seguro:
+            continue
